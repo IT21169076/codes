826a2d79d5c900a7e100949c5fe871a1d9ef6435
Shalini Muthumali Athukorala <99632841+IT21169076@users.noreply.github.com>
2022-12-19 10:32:44 +0530

    Add files via upload

--- q3.c
+++ q3.c
@@ -1,40 +1,41 @@
-#include <stdio.h>
-int main()
-{
-	int a[4][4]={{5,7,8,10},{9,3,0,6},{8,1,9,2},{4,7,2,1}};
-	int b[4][4]={{2,2,1,3},{8,1,3,1},{1,2,2,4},{3,1,1,2}};
-	int c[4][4];
-	int i,j;
-
-	printf("Enter 4*4 Values for matrix 1:\n");
-	for(i=0;i<4;i++){
-		for(j=0;j<4;j++){
-			printf(" %d",a[i][j]);
-		}
-		printf("\n");	
-	}
-		
-	printf("Enter 4*4 Values for matrix 2:\n");
-	for(i=0;i<=3;i++){
-		for(j=0;j<=3;j++){
-			printf(" %d",b[i][j]);
-		}
-		printf("\n");	
-	}
-		
-	for(i=0;i<4;i++)
-		for(j=0;j<4;j++)
-			c[i][j]=a[i][j]+b[i][j];
-		printf("\n");
-	printf("The value of matrixes are :\n");
-	for(i=0;i<4;i++){
-		for(j=0;j<4;j++){
-			printf("%6d",c[i][j]);
-		}
-		printf("\n");	
-	}
-       
-		
+#include<stdio.h>
+//boxes volume
+int main(){
 	
-
+	int boxes[4][3];
+	int volume[4];
+	int i,j;
+	
+	for(i=0;i<=3;i++){
+		printf("BOX %d : \n",i+1);
+		for(j=0;j<=2;j++){
+			printf("Enter size : ");
+			scanf("%d",&boxes[i][j]);
+		}
+	}
+	printf("Sizes : \n");
+	for(i=0;i<=3;i++){
+		for(j=0;j<=2;j++){
+			printf("%d ",boxes[i][j]);
+		}
+		printf("\n");
+	}
+	
+	for(i=0;i<=3;i++){
+		int vol=1;
+		int t;
+		for(j=0;j<=2;j++){
+			vol = vol*boxes[i][j];
+		}
+		volume[i]=vol;
+	
+	}
+	
+	printf("Volume of each Box : \n");
+	for(i=0;i<=3;i++){
+		printf("%d ",volume[i]);
+		printf("\n");
+	}
+	
+	return 0;
 }
